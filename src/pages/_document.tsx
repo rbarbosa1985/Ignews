1e47c81d2985fc5d826479ac7103ae35ee06f00d
Roberto Barbosa <betoanselmo@gmail.com>
2023-03-04 18:41:57 -0300

    Styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
+
       <body>
         <Main />
         <NextScript />
