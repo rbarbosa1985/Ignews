62c2cc767ec61d02037a1c3d11728d6854d6f543
Roberto Barbosa <betoanselmo@gmail.com>
2023-03-07 11:59:23 -0300

    API

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
 
   return isUserLoggedIn ? (
     <button type="button" className={styles.signInButton}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { SubscribeButton } from '@/components/SubscribeButton';
+import { stripe } from '@/services/stripe';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,12 +24,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(product.amount / 100)} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1MizGcJfwsnCZf7XPKokVHzd')
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount),
+  }
+
+
+  return {
+    props: {
+      product
+    }
+
+  }
 }
