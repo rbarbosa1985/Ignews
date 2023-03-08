da6c31a5e8f62f14dc652feeb76c2a3fedac1883
Roberto Barbosa <betoanselmo@gmail.com>
2023-03-07 21:39:35 -0300

    button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -8,13 +8,13 @@ export function SignInButton() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
 
   return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={() => { setIsUserLoggedIn(false) }}>
       <FaGithub color='#04d361' />
       Roberto Barbosa
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={() => { setIsUserLoggedIn(true) }}>
       <FaGithub color='#eba417' />
       Sign in with Github
     </button>
