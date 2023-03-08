cac68602c56b9cd74d86975f2aabec450978218b
Roberto Barbosa <betoanselmo@gmail.com>
2023-03-08 11:44:00 -0300

    GETSTATIC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { SubscribeButton } from '@/components/SubscribeButton';
 import { stripe } from '@/services/stripe';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import styles from './home.module.scss';
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1MizGcJfwsnCZf7XPKokVHzd')
 
   const product = {
@@ -49,7 +49,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
-
+    },
+    revalidate: 60 * 60 * 24, //  24 hours
   }
 }
