321cb92951f7c3b5b85c1093e2164a2eb951647a
Roberto Barbosa <betoanselmo@gmail.com>
2023-03-10 11:30:30 -0300

    Authentication Git Hub

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Header } from '@/components/Header';
 import type { AppProps } from 'next/app'
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 import '../styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
